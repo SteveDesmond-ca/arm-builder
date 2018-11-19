0ab7ae3f844d4c6eb3d18129c5273374469debc6
Steve Desmond <steve@vtsv.ca>
2018-11-18 21:51:57 -0500

    add common utils

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,24 @@ FROM ubuntu:xenial
 
 RUN apt-get update \
   && apt-get -y install \
+    apt-utils \
+    bash-completion \
+    bison \
     build-essential \
     curl \
     debootstrap \
     dos2unix \
     fakeroot \
+    flex \
+    gawk \
     gcc-arm-linux-gnueabihf \
     g++-arm-linux-gnueabihf \
     git \
     libsecret-1-dev \
+    libpython2.7 \
+    nano \
     qemu-user-static \
+    pkg-config-arm-linux-gnueabihf \
     python \
     rpm
     
--- setup.sh
+++ setup.sh
@@ -4,12 +4,17 @@ set -e
 qemu-debootstrap --arch=armhf --variant=minbase xenial /rootfs
 mount --bind /dev/pts /rootfs/dev/pts
 mount --bind /proc /rootfs/proc
-chroot /rootfs sh -c "apt-get update \
-    && apt-get install -y \
-        libglib2.0 \
-        libreadline-dev \
-        libsecret-1-dev \
-        libx11-dev \
-        libxkbfile-dev \
-        pkg-config \
-        zlib1g-dev"
+chroot /rootfs apt-get update
+chroot /rootfs apt-get install -y \
+    libglib2.0 \
+    libicu-dev \
+    libperl-dev \
+    libreadline-dev \
+    libsecret-1-dev \
+    libx11-dev \
+    libxkbfile-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    pkg-config \
+    tcl-dev \
+    zlib1g-dev
