d60af7659e83af0454f38a5b6c4c88b16659a3d2
Steve Desmond <steve@vtsv.ca>
2018-11-20 20:51:49 -0500

    add libpython-dev

--- base/setup.sh
+++ base/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -e
-
 qemu-debootstrap --arch=armhf --variant=minbase xenial /rootfs
-mount --bind /dev/pts /rootfs/dev/pts
-mount --bind /proc /rootfs/proc
--- builder/install.sh
+++ builder/install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+mount --bind /dev/pts /rootfs/dev/pts
+mount --bind /proc /rootfs/proc
+
 chroot /rootfs apt-get update
 chroot /rootfs apt-get install -y \
-    libglib2.0 \
+    apt-utils \
     libicu-dev \
+    libglib2.0 \
     libperl-dev \
+    libpython-dev \
     libreadline-dev \
     libsecret-1-dev \
     libx11-dev \
@@ -14,4 +19,4 @@ chroot /rootfs apt-get install -y \
     libxslt1-dev \
     pkg-config \
     tcl-dev \
-    zlib1g-dev
+    zlib1g-dev \
